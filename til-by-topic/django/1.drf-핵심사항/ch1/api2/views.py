6326e68f1929f347e21834bfed0645aa732683dc
sinkyoungdeok <tkagmd1@naver.com>
2022-09-07 00:20:53 +0900

    [ADD] (django) get post API에 serializer 안쓰고 직접 변환

--- til-by-topic/django/1.drf-핵심사항/ch1/api2/views.py
+++ til-by-topic/django/1.drf-핵심사항/ch1/api2/views.py
@@ -23,6 +23,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from api.utils import obj_to_post, prev_next_post, obj_to_comment
 from api2.serializers import CommentSerializer, PostListSerializer, PostRetrieveSerializer, \
     CateTagSerializer, PostSerializerDetail
 from blog.models import Post, Comment, Category, Tag
@@ -124,28 +125,43 @@ def get_prev_next(instance):
 
     return prev, next_
 class PostRetrieveAPIView(RetrieveAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializerDetail
+    # queryset = Post.objects.all()
+    # serializer_class = PostSerializerDetail
+    def get_queryset(self):
+        return Post.objects.all().select_related('category').prefetch_related('tags', 'comment_set')
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
-        prevInstance, nextInstance = get_prev_next(instance)
+        # prevInstance, nextInstance = get_prev_next(instance)
         commentList = instance.comment_set.all()
-        data = {
-            'post': instance,
-            'prevPost': prevInstance,
-            'nextPost': nextInstance,
-            'commentList': commentList,
-        }
-        serializer = self.get_serializer(instance = data)
-        return Response(serializer.data)
 
-    def get_serializer_context(self):
-        """
-        Extra context provided to the serializer class.
-        """
-        return {
-            'request': None,
-            'format': self.format_kwarg,
-            'view': self
+        postDict = obj_to_post(instance)
+        prevDict, nextDict = prev_next_post(instance)
+        commentDict = [obj_to_comment(c) for c in commentList]
+
+        dataDict = {
+            'post': postDict,
+            'prevPost': prevDict,
+            'nextPost': nextDict,
+            'commentList': commentDict,
         }
+
+        return Response(dataDict)
+        # data = {
+        #     'post': instance,
+        #     'prevPost': prevInstance,
+        #     'nextPost': nextInstance,
+        #     'commentList': commentList,
+        # }
+        # serializer = self.get_serializer(instance = data)
+        # return Response(serializer.data)
+
+    # def get_serializer_context(self):
+    #     """
+    #     Extra context provided to the serializer class.
+    #     """
+    #     return {
+    #         'request': None,
+    #         'format': self.format_kwarg,
+    #         'view': self
+    #     }
