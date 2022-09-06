c4f420d77577122c288a9c4db87f58d7c42fcef1
sinkyoungdeok <tkagmd1@naver.com>
2022-09-07 00:30:55 +0900

    [ADD] (django) api들을 viewset으로 리팩토링

--- til-by-topic/django/1.drf-핵심사항/ch1/api2/urls.py
+++ til-by-topic/django/1.drf-핵심사항/ch1/api2/urls.py
@@ -16,9 +16,25 @@ from django.urls import path
 from api2 import views
 
 urlpatterns = [
-    path('post/', views.PostListAPIView.as_view(), name='post-list'),
-    path('post/<int:pk>/', views.PostRetrieveAPIView.as_view(), name='post-detail'),
-    path('comment/', views.CommentCreateAPIView.as_view(), name='comment-list'),
-    path('post/<int:pk>/like/', views.PostLikeAPIView.as_view(), name='post-like'),
+    # path('post/', views.PostListAPIView.as_view(), name='post-list'),
+    # path('post/<int:pk>/', views.PostRetrieveAPIView.as_view(), name='post-detail'),
+    # path('comment/', views.CommentCreateAPIView.as_view(), name='comment-list'),
+    # path('post/<int:pk>/like/', views.PostLikeAPIView.as_view(), name='post-like'),
+    # path('catetag/', views.CateTagAPIView.as_view(), name='catetag'),
+
+    path('post/', views.PostViewSet.as_view(actions={
+        'get': 'list',
+    }), name='post-list'),
+    path('post/<int:pk>/', views.PostViewSet.as_view(actions={
+        'get': 'retrieve',
+    }), name='post-detail'),
+    path('post/<int:pk>/like/', views.PostViewSet.as_view(actions={
+        'get': 'like',
+    }), name='post-like'),
+
+    path('comment/', views.CommentViewSet.as_view(actions={
+        'post': 'create',
+    }), name='comment-list'),
+
     path('catetag/', views.CateTagAPIView.as_view(), name='catetag'),
 ]
--- til-by-topic/django/1.drf-핵심사항/ch1/api2/views.py
+++ til-by-topic/django/1.drf-핵심사항/ch1/api2/views.py
@@ -1,27 +1,10 @@
-# from django.contrib.auth.models import User
-# from rest_framework import viewsets
-#
-# from api2.serializers import UserSerializer, PostSerializer, CommentSerializer
-# from blog.models import Post, Comment
-#
-#
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#
-# class PostViewSet(viewsets.ModelViewSet):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-#
-# class CommentViewSet(viewsets.ModelViewSet):
-#     queryset = Comment.objects.all()
-#     serializer_class = CommentSerializer
 from collections import OrderedDict
 
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, GenericAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from api.utils import obj_to_post, prev_next_post, obj_to_comment
 from api2.serializers import CommentSerializer, PostListSerializer, PostRetrieveSerializer, \
@@ -29,48 +12,9 @@ from api2.serializers import CommentSerializer, PostListSerializer, PostRetrieve
 from blog.models import Post, Comment, Category, Tag
 
 
-# class PostListAPIView(ListAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostListSerializer
-
-
-# class PostRetrieveAPIView(RetrieveAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostRetrieveSerializer
-
-
-class CommentCreateAPIView(CreateAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-
-# class PostLikeAPIView(UpdateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostLikeSerializer
-#     # PATCH method
-#     def update(self, request, *args, **kwargs):
-#         partial = kwargs.pop('partial', False)
-#         instance = self.get_object()
-#         data = {'like': instance.like + 1}
-#         serializer = self.get_serializer(instance, data=data, partial=partial)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_update(serializer)
-#
-#         if getattr(instance, '_prefetched_objects_cache', None):
-#             # If 'prefetch_related' has been applied to a queryset, we need to
-#             # forcibly invalidate the prefetch cache on the instance.
-#             instance._prefetched_objects_cache = {}
-#
-#         return Response(data['like'])
-
-class PostLikeAPIView(GenericAPIView):
-    queryset = Post.objects.all()
-    def get(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.like += 1
-        instance.save()
-
-
-        return Response(instance.like)
+# class CommentCreateAPIView(CreateAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
 
 class CateTagAPIView(APIView):
     def get(self, request, *args, **kwargs):
@@ -96,8 +40,7 @@ class PostPageNumberPagination(PageNumberPagination):
             ('curPage', self.page.number),
         ]))
 
-
-class PostListAPIView(ListAPIView):
+class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
     pagination_class = PostPageNumberPagination
@@ -112,21 +55,6 @@ class PostListAPIView(ListAPIView):
             'view': self
         }
 
-def get_prev_next(instance):
-    try:
-        prev = instance.get_previous_by_update_dt()
-    except instance.DoesNotExist:
-        prev = None
-
-    try:
-        next_ = instance.get_next_by_update_dt()
-    except instance.DoesNotExist:
-        next_ = None
-
-    return prev, next_
-class PostRetrieveAPIView(RetrieveAPIView):
-    # queryset = Post.objects.all()
-    # serializer_class = PostSerializerDetail
     def get_queryset(self):
         return Post.objects.all().select_related('category').prefetch_related('tags', 'comment_set')
 
@@ -147,21 +75,15 @@ class PostRetrieveAPIView(RetrieveAPIView):
         }
 
         return Response(dataDict)
-        # data = {
-        #     'post': instance,
-        #     'prevPost': prevInstance,
-        #     'nextPost': nextInstance,
-        #     'commentList': commentList,
-        # }
-        # serializer = self.get_serializer(instance = data)
-        # return Response(serializer.data)
 
-    # def get_serializer_context(self):
-    #     """
-    #     Extra context provided to the serializer class.
-    #     """
-    #     return {
-    #         'request': None,
-    #         'format': self.format_kwarg,
-    #         'view': self
-    #     }
+    def like(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.like += 1
+        instance.save()
+
+        return Response(instance.like)
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
