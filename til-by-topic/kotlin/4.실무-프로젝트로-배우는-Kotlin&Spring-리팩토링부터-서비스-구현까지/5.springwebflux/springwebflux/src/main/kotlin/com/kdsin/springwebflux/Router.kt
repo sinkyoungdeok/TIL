a8dfca34d4be952bd2b87c9fb769d881198ac3e6
sinkyoungdeok <tkagmd1@naver.com>
2022-11-24 22:00:12 +0900

    [ADD] (kotlin) 함수형 엔드포인트

--- til-by-topic/kotlin/4.실무-프로젝트로-배우는-Kotlin&Spring-리팩토링부터-서비스-구현까지/5.springwebflux/springwebflux/src/main/kotlin/com/kdsin/springwebflux/Router.kt
+++ til-by-topic/kotlin/4.실무-프로젝트로-배우는-Kotlin&Spring-리팩토링부터-서비스-구현까지/5.springwebflux/springwebflux/src/main/kotlin/com/kdsin/springwebflux/Router.kt
@@ -5,13 +5,23 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.RouterFunctions
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
 
 @Configuration
 class Router {
 
     @Bean
-    fun helloRouter(handler: HelloHandler) : RouterFunction<ServerResponse> =
+    fun helloRouter(handler: HelloHandler): RouterFunction<ServerResponse> =
         RouterFunctions.route()
             .GET("/", handler::sayHello)
             .build()
+
+    @Bean
+    fun userRouter(handler: UserHandler): RouterFunction<ServerResponse> =
+        router {
+            "/users".nest {
+                GET("/{id}", handler::getUser)
+                GET("", handler::getAll)
+            }
+        }
 }
---
+++ til-by-topic/kotlin/4.실무-프로젝트로-배우는-Kotlin&Spring-리팩토링부터-서비스-구현까지/5.springwebflux/springwebflux/src/main/kotlin/com/kdsin/springwebflux/UserHandler.kt
@@ -0,0 +1,26 @@
+package com.kdsin.springwebflux
+
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+
+data class User(val id: Long, val email: String)
+
+@Component
+class UserHandler {
+
+    val users = listOf(
+        User(id = 1, email = "user1@gmail.com"),
+        User(id = 2, email = "user2@gmail.com"),
+    )
+
+    fun getUser(req: ServerRequest): Mono<ServerResponse> =
+        users.find { req.pathVariable("id").toLong() == it.id }
+            ?.let {
+                ServerResponse.ok().bodyValue(it)
+            } ?: ServerResponse.notFound().build()
+
+    fun getAll(req: ServerRequest): Mono<ServerResponse> =
+        ServerResponse.ok().bodyValue(users)
+}
