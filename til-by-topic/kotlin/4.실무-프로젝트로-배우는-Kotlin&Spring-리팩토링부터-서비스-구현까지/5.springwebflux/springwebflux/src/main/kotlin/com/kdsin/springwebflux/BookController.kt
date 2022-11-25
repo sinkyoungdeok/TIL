771e2a2d24b7d12266d2a594700a34d34b89fac0
Bob Sin <tkagmd1@naver.com>
2022-11-25 19:58:36 +0900

    [ADD] (kotlin) 애노테이션 컨트롤러

---
+++ til-by-topic/kotlin/4.실무-프로젝트로-배우는-Kotlin&Spring-리팩토링부터-서비스-구현까지/5.springwebflux/springwebflux/src/main/kotlin/com/kdsin/springwebflux/BookController.kt
@@ -0,0 +1,31 @@
+package com.kdsin.springwebflux
+
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@RestController
+class BookController(
+    private val bookService: BookService
+) {
+
+    @GetMapping("/books")
+    fun getAll(): Flux<Book> {
+        return bookService.getAll()
+    }
+
+    @GetMapping("/books/{id}")
+    fun get(@PathVariable id: Int): Mono<Book> {
+        return bookService.get(id)
+    }
+
+    @PostMapping("/books")
+    fun add(@RequestBody request: Map<String, Any>): Mono<Book> {
+        return bookService.add(request)
+    }
+
+    @DeleteMapping("/books/{id}")
+    fun delete(@PathVariable id: Int): Mono<Void> {
+        return bookService.delete(id)
+    }
+}
---
+++ til-by-topic/kotlin/4.실무-프로젝트로-배우는-Kotlin&Spring-리팩토링부터-서비스-구현까지/5.springwebflux/springwebflux/src/main/kotlin/com/kdsin/springwebflux/BookService.kt
@@ -0,0 +1,50 @@
+package com.kdsin.springwebflux
+
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
+import java.util.concurrent.atomic.AtomicInteger
+
+data class Book(val id: Int, val name: String, val price: Int)
+
+@Service
+class BookService {
+
+    private final val nextId = AtomicInteger(0)
+
+    val books = mutableListOf(
+        Book(id = nextId.incrementAndGet(), name = "코틀린 인 액션", price = 30000),
+        Book(id = nextId.incrementAndGet(), name = "HTTP 완벽 가이드", price = 40000),
+    )
+
+    fun getAll(): Flux<Book> {
+//        return Flux.fromIterable(books)
+        return books.toFlux()
+    }
+
+    fun get(id: Int): Mono<Book> {
+//        return Mono.justOrEmpty(books.find { it.id == id })
+        return books.find { it.id == id }.toMono()
+    }
+
+    fun add(request: Map<String, Any>): Mono<Book> {
+        return Mono.just(request)
+            .map { map ->
+                val book = Book(
+                    id = nextId.incrementAndGet(),
+                    name = map["name"].toString(),
+                    price = map["price"] as Int
+                )
+                books.add(book)
+                book
+            }
+    }
+
+    fun delete(id: Int): Mono<Void> {
+        return Mono.justOrEmpty(books.find { it.id == id})
+            .map { books.remove(it) }
+            .then()
+    }
+}
