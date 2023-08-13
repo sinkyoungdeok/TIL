daca2a83181d33c6cfe538be0d15fc071054e798
sinkyoungdeok <tkagmd1@naver.com>
2023-08-13 17:38:01 +0900

    [ADD] (react) 12. useRef 로 컴포넌트 안의 변수 만들기

--- til-by-topic/react/begin-react/src/App.js
+++ til-by-topic/react/begin-react/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import UserList from './UserList';
 
 function App() {
-    return (
-        <UserList/>
-    );
+    const users = [
+        {
+            id: 1,
+            username: 'tlsrudejr',
+            email: 'tlsrudejr123@gmail.com'
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com'
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com'
+        }
+    ];
+
+    const nextId = useRef(4);
+    const onCreate = () => {
+        nextId.current += 1;
+    }
+    return <UserList users={users} />;
 }
 
 export default App;
--- til-by-topic/react/begin-react/src/UserList.js
+++ til-by-topic/react/begin-react/src/UserList.js
@@ -8,28 +8,11 @@ function User({user}) {
     )
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'tlsrudejr',
-            email: 'tlsrudejr123@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
+function UserList({users}) {
     return (
         <div>
-            {users.map((user, index) => (
-                <User user={user} key={index}/>
+            {users.map(user => (
+                <User user={user} key={user.id}/>
             ))}
         </div>
     )
